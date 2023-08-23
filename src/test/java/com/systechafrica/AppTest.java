58fe4431079e315e8bdbdcc69cac582af386d2ed
Tonny Oseko <tonnyoseko0@gmail.com>
2023-08-23 14:48:42 +0300

    Add tests

--- src/test/java/com/systechafrica/AppTest.java
+++ src/test/java/com/systechafrica/AppTest.java
@@ -2,7 +2,9 @@ package com.systechafrica;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class AppTest {
     App app = new App();
 
