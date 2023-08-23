ed117b15e40c51e86956462a2940f89765ef7f38
Tonny Oseko <tonnyoseko0@gmail.com>
2023-08-23 14:02:47 +0300

    Add tests

--- src/main/java/com/systechafrica/App.java
+++ src/main/java/com/systechafrica/App.java
@@ -4,10 +4,12 @@ package com.systechafrica;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    public int add(int a, int b) {
+        return a + b;
     }
 }
--- src/test/java/com/systechafrica/AppTest.java
+++ src/test/java/com/systechafrica/AppTest.java
@@ -1,20 +1,19 @@
 package com.systechafrica;
 
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
+public class AppTest {
+    App app = new App();
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void add() {
+        // when
+        int result = app.add(1, 2);
+        // then
+        int expected = 3;
+        // verify
+        Assertions.assertEquals(expected, result, "1 + 2 should be 3");
+
     }
 }
