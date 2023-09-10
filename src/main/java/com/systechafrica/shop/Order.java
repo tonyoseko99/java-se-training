642e0e43ecc270b94c08ee9df1a2f036ee5ae793
Tonny Oseko <tonnyoseko0@gmail.com>
2023-09-11 00:08:30 +0300

    method to order food / drinks

--- src/main/java/com/systechafrica/shop/Order.java
+++ src/main/java/com/systechafrica/shop/Order.java
@@ -1,5 +1,20 @@
 package com.systechafrica.shop;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Order {
-    
+    private List<Item> orderedItems;
+
+    public Order() {
+        orderedItems = new ArrayList<>();
+    }
+
+    public void addItem(Item item) {
+        orderedItems.add(item);
+    }
+
+    public List<Item> getOrderedItems() {
+        return orderedItems;
+    }
 }
