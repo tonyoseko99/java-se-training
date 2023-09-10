afd8effe6a3f57ac1a38e736c93af732409d5e15
Tonny Oseko <tonnyoseko0@gmail.com>
2023-09-11 00:09:51 +0300

    process payments

--- src/main/java/com/systechafrica/shop/Payment.java
+++ src/main/java/com/systechafrica/shop/Payment.java
@@ -1,5 +1,36 @@
 package com.systechafrica.shop;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class Payment {
-    
+    public static void processPayment(List<Item> orderedItems) {
+        double totalCost = calculateTotalCost(orderedItems);
+
+        System.out.println("Total Cost: $" + totalCost);
+
+        Scanner scanner = new Scanner(System.in);
+        double amountPaid;
+
+        do {
+            System.out.print("Enter the amount paid: $");
+            amountPaid = scanner.nextDouble();
+
+            if (amountPaid < totalCost) {
+                System.out.println("Amount paid is less than the total cost. Please enter a sufficient amount.");
+            }
+        } while (amountPaid < totalCost);
+
+        double change = amountPaid - totalCost;
+        System.out.println("Change: $" + change);
+        System.out.println("Payment received. Thank you!");
+    }
+
+    private static double calculateTotalCost(List<Item> orderedItems) {
+        double totalCost = 0;
+        for (Item item : orderedItems) {
+            totalCost += item.getPrice();
+        }
+        return totalCost;
+    }
 }
