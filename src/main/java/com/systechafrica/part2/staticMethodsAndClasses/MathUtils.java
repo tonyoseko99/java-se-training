89df5b531e30f9bd8c553c274a2f53becd0348dc
Tonny Oseko <tonnyoseko0@gmail.com>
2023-09-19 06:10:39 +0300

    Static Classes and Methods

--- src/main/java/com/systechafrica/part2/staticMethodsAndClasses/MathUtils.java
+++ src/main/java/com/systechafrica/part2/staticMethodsAndClasses/MathUtils.java
@@ -18,4 +18,14 @@ public class MathUtils {
             return a / b;
         }
     }
+    public static class Area{
+        static int length = 5;
+        static int width = 10;
+        static int area = length * width;   //! if you fail to declare this as static, you will get an error in the getArea method
+                                            //! because you cannot access a non-static variable from a static method
+
+        public static int getArea(){
+            return area;
+        }
+    }
 }
