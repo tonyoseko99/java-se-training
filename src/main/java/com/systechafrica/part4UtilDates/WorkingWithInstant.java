44c40900050994d0fe497d637186ca4071d23b40
Tonny Oseko <tonnyoseko0@gmail.com>
2023-10-03 07:03:36 +0300

    Working with dates

---
+++ src/main/java/com/systechafrica/part4UtilDates/WorkingWithInstant.java
@@ -0,0 +1,20 @@
+package com.systechafrica.part4UtilDates;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.TimeZone;
+
+public class WorkingWithInstant {
+    public static void main(String[] args) {
+        Instant now = Instant.now();
+        System.out.println("instant: " + now);
+        System.out.println("now.getEpochSecond(): " + LocalDateTime.now());
+        System.out.println("now.getEpochSecond(): " + now.getEpochSecond());
+
+        TimeZone tz = TimeZone.getDefault();
+        System.out.println("tz: " + tz);
+        System.out.println("tz.getID(): " + tz.getID());
+        System.out.println("tz.getDisplayName(): " + tz.getDisplayName());
+
+    }
+}
